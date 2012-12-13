f2c0830372165d13694fbc65d10d7295441f35e7
Carlos Mapua <carlos.mapua@gmail.com>
2012-12-13 13:57:01 +0800

    made changes to camera behaviour

--- Mythos High/Assets/Resources/Scripts/CameraLookat.cs
+++ Mythos High/Assets/Resources/Scripts/CameraLookat.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class CameraLookat : MonoBehaviour {
 	public Transform target;
+	public float rotSpeed = 5;
+	public float transSpeed = 2;
 	//cached
 	private Transform myTransform;
+	
 	// Use this for initialization
 	void Start () {
 		myTransform = transform;
@@ -14,7 +17,12 @@ public class CameraLookat : MonoBehaviour {
 	void Update () {
 		//fancy smooth lookAt
 		Quaternion lookRotation = Quaternion.LookRotation(target.position - myTransform.position);
-		float str = Mathf.Min(5 * Time.deltaTime, 1);
-		myTransform.rotation = Quaternion.Lerp(myTransform.rotation, lookRotation, str);
+		float str = Mathf.Min(transSpeed * Time.deltaTime, 1);
+		float rotStr = Mathf.Min(rotSpeed * Time.deltaTime, 1);
+		myTransform.rotation = Quaternion.Lerp(myTransform.rotation, lookRotation, rotStr);
+		
+		//fancy smooth follow
+		Vector3 xDiff = new Vector3(target.position.x, myTransform.position.y, myTransform.position.z);
+		myTransform.position = Vector3.Lerp(myTransform.position, xDiff, str);
 	}
 }
