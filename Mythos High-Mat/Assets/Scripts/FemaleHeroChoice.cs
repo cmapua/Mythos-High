29486e80f8796179c6c846b63923131eabb08799
mat <mat_tabios@yahoo.com>
2013-02-19 07:58:39 +0800

    More

---
+++ Mythos High-Mat/Assets/Scripts/FemaleHeroChoice.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class FemaleHeroChoice : MonoBehaviour {
+
+	public GUIStyle customButton;
+	
+	void OnGUI () {
+		if (GUI.Button (new Rect ((Screen.width/2)-175,(Screen.height/2),150,100), "Anansi")) 
+		{
+			Application.LoadLevel ("");
+		}
+		if (GUI.Button (new Rect ((Screen.width/2)-175,(Screen.height/2)+100,150,100), "Aphrodite")) 
+		{
+			Application.LoadLevel ("");
+		}
+		if (GUI.Button (new Rect ((Screen.width/2)-175,(Screen.height/2)+200,150,100), "Buddha")) 
+		{
+			Application.LoadLevel ("");
+		}
+			if (GUI.Button (new Rect ((Screen.width/2)+25,(Screen.height/2),150,100), "Osiris")) 
+		{ 
+			Application.LoadLevel ("");
+		}
+		if (GUI.Button (new Rect ((Screen.width/2)+25,(Screen.height/2)+100,150,100), "Shiva")) 
+		{
+			Application.LoadLevel ("");
+		}
+		if (GUI.Button (new Rect ((Screen.width/2)+25,(Screen.height/2)+200,150,100), "Thor")) 
+		{
+			Application.LoadLevel ("");
+		}
+	}
+}
