ca33733a40ff66afd4a566c1c485e146dd1978fb
Sam Chan <samlorrainechan@gmail.com>
2013-02-28 13:14:53 +0800

    add buttons to start screen

--- Mythos High-Mat/Assets/Scripts/StartScreen.cs
+++ Mythos High-Mat/Assets/Scripts/StartScreen.cs
@@ -3,18 +3,16 @@ using System.Collections;
 
 public class StartScreen : MonoBehaviour {
 
-	public GUIStyle customButton;
+	public GUIStyle startButton;
+	public GUIStyle exitButton;
 	
 	void OnGUI () {
-		if (GUI.Button (new Rect ((Screen.width/2)-75,(Screen.height/2),150,100), "Start Game")) {
+		if (GUI.Button (new Rect ((Screen.width/2)-75,(Screen.height/2),150,100), "", startButton)) {
 			 
 
 			Application.LoadLevel ("Opening Dialog");
 		}
-		if (GUI.Button (new Rect ((Screen.width/2)-75,(Screen.height/2)+100,150,100), "Options")) {
-			
-		}
-		if (GUI.Button (new Rect ((Screen.width/2)-75,(Screen.height/2)+200,150,100), "Exit")) {
+		if (GUI.Button (new Rect ((Screen.width/2)-75,(Screen.height/2)+200,150,100), "", exitButton)) {
 			Application.Quit();
 		}
 	}
