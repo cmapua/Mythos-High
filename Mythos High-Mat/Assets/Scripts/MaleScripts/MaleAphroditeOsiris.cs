1a00544b152d5f77e91f77c3a8ed2e591b8996f1
mat <mat_tabios@yahoo.com>
2013-03-01 10:48:46 +0800

    additional

--- Mythos High-Mat/Assets/Scripts/MaleScripts/MaleAphroditeOsiris.cs
+++ Mythos High-Mat/Assets/Scripts/MaleScripts/MaleAphroditeOsiris.cs
@@ -3,15 +3,30 @@ using System.Collections;
 
 public class MaleAphroditeOsiris : MonoBehaviour {
 
-	public GUIStyle customButton;
+	public GUIStyle AphroditeItem;
+	public GUIStyle OsirisItem;
+	public GUIStyle Aphrodite;
+	public GUIStyle Osiris;
 	
 	void OnGUI () {
-		 GUI.Label(new Rect((Screen.width/2)-75,(Screen.height/2),400,100), "Choose Wisely!!!");
-		if (GUI.Button (new Rect ((Screen.width/2)-75,(Screen.height/2)+100,400,100), "A) Hey, the Bangle is pretty.")) {
+
+		GUI.Label(new Rect((Screen.width/2)-75,(Screen.height/2)-400,400,100), "Choose Wisely!!!");
+		GUI.Label(new Rect((Screen.width/2)-350,(Screen.height/2)-200,200,400), "",Aphrodite);
+		if (GUI.Button (new Rect ((Screen.width/2)-150,(Screen.height/2)-200,200,400), 
+			"",AphroditeItem)) 
+		{
 			Application.LoadLevel ("MaleAphroditeDialogue");
 		}
-		if (GUI.Button (new Rect ((Screen.width/2)-75,(Screen.height/2)+200,400,100), "B) I’ll go with the Crook. It’s more sensible.")) {
+		GUI.Label(new Rect((Screen.width/2)+350,(Screen.height/2)-200,200,400), "",Osiris);
+		if (GUI.Button (new Rect ((Screen.width/2)+150,(Screen.height/2)-200,200,400),
+			"",OsirisItem)) 
+		{
 			Application.LoadLevel ("MaleOsirisDialogue");
 		}
+		GUI.Label(new Rect((Screen.width/2)-200,(Screen.height/2)+300,400,100), 
+			"Hey, the Bangle is pretty.");
+		GUI.Label(new Rect((Screen.width/2)+200,(Screen.height/2)+300,400,100), 
+			"Ill go with the Crook. Its more sensible.");
 	}
 }
+
